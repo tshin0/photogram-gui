6095b248dbf006d01f351e6916ed14e9ca342066
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-11-14 20:52:19 +0000

    Added user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
 
   def index
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :username => :asc })
+    
     render({ :template => "user_templates/index" })
   end 
 
+
+  def show
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.at(0)
+
+  
+
+    render({ :template => "user_templates/show" })
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
+
 end
